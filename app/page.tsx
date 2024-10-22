9e564c81a90ed6e2713d7351b1a33b8cffbdcacf
Marty McGee <support@companyjuice.com>
2024-10-22 11:40:48 -0700

    v0.1.2: settings + branding

--- app/page.tsx
+++ app/page.tsx
@@ -26,7 +26,7 @@ export default async function Home() {
   return (
     <>
       <div className="z-10 w-full max-w-xl px-5 xl:px-0">
-        <a
+        {/* <a
           href="https://twitter.com/martymcgee/status/1613928948915920896"
           target="_blank"
           rel="noreferrer"
@@ -36,12 +36,13 @@ export default async function Home() {
           <p className="text-sm font-semibold text-[#1d9bf0]">
             Introducing ThreeD Garden
           </p>
-        </a>
+        </a> */}
         <h1
           className="animate-fade-up bg-gradient-to-br from-black to-stone-500 bg-clip-text text-center font-display text-4xl font-bold tracking-[-0.02em] text-transparent opacity-0 drop-shadow-sm [text-wrap:balance] md:text-7xl md:leading-[5rem]"
           style={{ animationDelay: "0.15s", animationFillMode: "forwards" }}
         >
-          ThreeD Garden on 3D FarmBot
+          ThreeD Garden
+          {/* on 3D FarmBot */}
         </h1>
         <p
           className="mt-6 animate-fade-up text-center text-gray-500 opacity-0 [text-wrap:balance] md:text-xl"
